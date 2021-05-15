9dd1520f20d8bfb8508a53d2313c5c3215d3f14f
Nadir Chowdhury <chowdhurynadir0@outlook.com>
2021-05-15 15:09:28 +0100

    Update client.pyi

--- github/client.pyi
+++ github/client.pyi
@@ -1,7 +1,8 @@
 from aiohttp import ClientSession
+from typing import Text
 
 
 class Client:
-    def __init__(self, token: str, *, session: ClientSession, user_agent: str=...) -> None: ...
+    def __init__(self, token: Text, *, session: ClientSession, user_agent: str=...) -> None: ...
 
     async def request(self, query: str, operation: str=..., variables: dict=...) -> dict: ...
